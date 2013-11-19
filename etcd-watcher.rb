fc1d23299c1f65a768d2600a14c6abcfadfa3148
Jay Faulkner <jay@jvf.cc>
2013-11-19 10:38:48 -0800

    handle signals

--- etcd-watcher.rb
+++ etcd-watcher.rb
@@ -7,7 +7,8 @@ require 'etcd'
 require 'optparse'
 require 'logger'
 
-logger = Logger.new(STDOUT)
+@logger = Logger.new(STDOUT)
+@logger.level = Logger::INFO
 
 args = ARGV.dup
 options = {}
@@ -37,57 +38,64 @@ optparse = OptionParser.new do |o|
     options[:uri] = u
   end
 
-  ## TODO: Make this toggle loglevel
-  o.on('-v', '--[no-]verbose', 'Run in verbose mode. LOTS of output.') do |v|
-    options[:verbose] = v
+  o.on('-v', '--[no-]verbose', 'Run in verbose mode: log at debug') do |v|
+    @logger.level = Logger::DEBUG if v
   end
 
   o.on_tail("-h", "--help", "Show this message") { puts o; exit }
+
+  # TODO: Parameters for TTL and how often to heartbeat
 end
 
-retry_interval = options.has_key?(:retry_interval) ? options[:retry_interval] : 30
-
 optparse.parse!(args)
+
+retry_interval = options.has_key?(:retry_interval) ? options[:retry_interval] : 30
 
 @etcd = {}
 @etcd[:uris] = options.has_key?(:uri) ? options[:uri] : ['http://127.0.0.1:4001']
 @etcd[:heartbeat_freq] = options.has_key?(:hb) ? options[:hb].to_f : 10.0
 
 def etcd_connect
+  @logger.debug "Connecting etcd client with options #{@etcd.inspect}"
   @client = Etcd::Client.new(@etcd)
   @client.connect
 end
 
-def terminate_pid(pid)
-  Process.kill(pid, 15)
-  Process.wait
-end
-
+@logger.debug "Spawning process #{options[:cmd]}"
 process = Process.spawn(options[:cmd])
-pid = process.to_i
+@pid = process.to_i
 
-trap("CLD") {
-  Process.wait
-  status = $?.exitstatus
-  puts "Child pid #{$?.pid}: died with status #{status}, terminating supervisor" if options[:verbose]
-  @client.delete(options[:key])
-  exit status
+trap(17) {
+  begin
+    @client.delete(options[:key])
+    Process.wait
+    status = $?.exitstatus.nil ? "unknown" : $?.exitstatus
+    process = $?.pid.nil? ? "unknown" : $?.pid
+    exit status
+  ensure
+    exit 0
+  end
 }
 
-#trap("INT") {
-#  logger.error "Interrupted!"
-#  Process.kill(pid, 2)
-#  Process.wait
-#  logger.error "lol"
-#}
+trap(2) {
+  begin
+    Process.kill(2, @pid)
+    Process.wait
+    if $?.exitstatus.nil? then exit 1 else exit $?.exitstatus end
+  ensure
+    exit 1
+  end
+}
 
-trap("TERM") {
-  @client.delete(options[:key])
-  puts "Sending SIGTERM to #{pid}" if options[:verbose]
-  Process.kill(pid, 15)
-  Process.wait
-  puts "Exiting on SIGTERM" if options[:verbose]
-  exit 0
+trap(15) {
+  begin
+    @client.delete(options[:key])
+    Process.kill(15, @pid)
+    Process.wait
+    if $?.exitstatus.nil? then exit 0 else exit $?.exitstatus end
+  ensure
+    exit 0
+  end
 }
 
 etcd_connect
@@ -98,14 +106,18 @@ begin
     sleep 15
   end
 rescue Etcd::EtcdError => e
-  logger.error "Etcd error #{e.class} encounter! Will attempt reconnect in #{retry_interval} seconds."
+  @logger.error "Etcd error #{e.class} encounter! Will attempt reconnect in " +
+               "#{retry_interval} seconds."
   sleep retry_interval
   etcd_connect
   retry
 rescue => e
-  # I don't like doing this, but occasionally etcd-rb throws something other than an EtcdError
-  # when the etcd server goes away.
-  logger.error "Unhandled exception: #{e.class} #{e}"
+  # I don't like doing this, but occasionally etcd-rb throws something other
+  # than an EtcdError when the etcd server goes away mid-request. Otherwise if
+  # the exception gets thrown, the child process gets reparented and we stop
+  # heartbeating
+  @logger.error "Unhandled exception: #{e.class} #{e}, Will attempt reconnect " +
+               "to etcd in #{retry_interval} seconds."
   sleep retry_interval
   etcd_connect
   retry
